2186eae7e7b68ed13a6443c9392ef98fbcf7ca8a
Yaseen Shaikh <yaseenshaik9066@gmail.com>
2018-09-11 21:22:25 +0530

    delet to test
    new test update to master

--- README.md
+++
@@ -1,2 +0,0 @@
-# yaseenshaikh
-My webpage for Demo.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,162 @@
-(function($) {
-  
-  "use strict";  
+/*
+ * Evento -  Event html  Template
+ * Build Date: jan 2018
+ * Author: colorlib
+ * Copyright (C) 2018 colorlib
+ */
+ /* ------------------------------------- */
+/*  TABLE OF CONTENTS
+ /* ------------------------------------- */
+/*   PRE LOADING                          */
+/*   WOW                                 */
+/*   sliders                      */
+/*    MAPS                               */
+/*   COUNTER JS              */
 
-  $(window).on('load', function() {
 
-     /* Page Loader active
-    ========================================================*/
-    $('#preloader').fadeOut();
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 200) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
+    /* ==============================================
+/*  PRE LOADING
+  =============================================== */
+'use strict';
+$(window).load(function() {
+    $('.loader').delay(500).fadeOut('slow');
+});
+
+
+$(document).ready(function() {
+
+    'use strict';
+    /* ==============================================
+     /*   wow
+      =============================================== */
+    var wow = new WOW(
+        {
+            animateClass: 'animated',
+            offset: 10,
+            mobile: true
         }
-    });
-
-    /* slicknav mobile menu active  */
-    $('.mobile-menu').slicknav({
-      prependTo: '.navbar-header',
-      parentTag: 'liner',
-      allowParentLinks: true,
-      duplicate: true,
-      label: '',
-      closedSymbol: '<i class="icon-arrow-right"></i>',
-      openedSymbol: '<i class="icon-arrow-down"></i>',
-    });
-
-    /* ==========================================================================
-    countdown timer
-    ========================================================================== */
-     jQuery('#clock').countdown('2018/10/19',function(event){
-      var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
-      +'<div class="time-entry hours"><span>%H</span> Hours</div> '
-      +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
-      +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
-    });
-
-    /* WOW Scroll Spy
-    ========================================================*/
-     var wow = new WOW({
-      //disabled for mobile
-        mobile: false
-    });
+    );
     wow.init();
+    /* ==============================================
+        STICKY HEADER
+        =============================================== */
 
-    // one page navigation 
-    $('.onepage-nev').onePageNav({
-            currentClass: 'active'
-    }); 
-
-    /* Back Top Link active
-    ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() < 100) {
+            $('.header').removeClass('sticky_header');
         } else {
-          $('.back-to-top').fadeOut(400);
+            $('.header').addClass('sticky_header');
         }
-      });
+    });
+    /* --------------------------------------------------------
+     COUNTER JS
+     ----------------------------------------------------------- */
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+    $('.counter').counterUp({
+        delay: 5,
+        time: 3000
+    });
 
-  });      
+    $(".countdown")
+        .countdown("2018/03/01", function(event) {
+            $(this).html(
+                event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
+            );
+        });
 
-}(jQuery));
+    /* ==============================================
+     SLIDER
+     =============================================== */
+    $(".cover_slider").owlCarousel({
+        loop:true,
+        autoplay:true,
+        smartSpeed:1000,
+        autoplayHoverPause:false,
+        dots:true,
+        nav:false,
+        items:1,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        dotsContainer: '.cover_dots'
+    });
+
+    $(".brand_carousel").owlCarousel({
+        loop:true,
+        autoplay:true,
+        smartSpeed:450,
+        autoplayHoverPause:false,
+        dots:false,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+
+            },
+            1000:{
+                items:5
+
+            }
+        },
+        items:5
+    });
+    /* ------------------------------------- */
+    /* Animated progress bars
+     /* ------------------------------------- */
+
+    var waypoints = $('.progress_container').waypoint(function() {
+        $('.progress .progress-bar').progressbar({
+            transition_delay: 1000
+        });
+    },{
+        offset: '50%'
+    });
+
+
+        /* --------------------------------------------------------
+    MAPS
+    ----------------------------------------------------------- */
+    var map = $('#map');
+    if(map.length > 0) {
+        google.maps.event.addDomListener(window, 'load', init);
+        var lattuide = map.attr('data-lat');
+        var longtuided = map.attr('data-lon');
+    }
+    function init() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 16,
+            scrollwheel: false,
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(lattuide, longtuided), // New York
+
+            // How you would like to style the map.
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
+        };
+
+        // Get the HTML DOM element that will contain your map
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lattuide, longtuided),
+            map: map,
+            title: 'evento!'
+        });
+    }
+
+});
